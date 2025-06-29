9ef0530b7a41eb7e203939ddd01ab5471dc7107a
Sivareddyvk <144054062+Sivareddyvk@users.noreply.github.com>
2025-06-29 11:41:06 +0530

    Update TodoApplication.java

--- backend/src/main/java/com/example/todo/TodoApplication.java
+++ backend/src/main/java/com/example/todo/TodoApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class TodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
     }
+    @GetMapping("/")
+    public String toStrng() {
+        return "This is Spring Boot Application";
 }
